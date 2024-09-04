2e5b58f0f01ecf3370c63fdf4aa932759170a73b
ToeOoWaiYan <toeoowaiyan4252@gmail.com>
2024-09-04 11:30:07 +0630

    resend email error

--- backend/src/controllers/User/EmailVerifyController.js
+++ backend/src/controllers/User/EmailVerifyController.js
@@ -60,7 +60,7 @@ module.exports = {
       return res.status(400).json({ msg: "Email already verified" });
     await EmailVerificationToken.destroy({ where: { user_id: user.id } });
 
-    const verificationToken = await generateEmailVerificationToken({user_id: user.id, email: user.email});
+    const verificationToken = await generateEmailVerificationToken(user);
     if (verificationToken) {
       sendEmailQueue.add({ email: user.email, url: verificationToken.url });
       return res.json({ msg: "Verification email resent" });
