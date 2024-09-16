315c8fb910aab7c4f044ed7522355c3877c39637
winhtutaung1234 <hh4626783@gmail.com>
2024-09-16 11:40:13 +0630

    changes some routers name

--- backend/src/controllers/Transcation/TranscationController.js
+++ backend/src/controllers/Transcation/TranscationController.js
@@ -1,5 +1,9 @@
+require("dotenv").config();
+
 const asyncHandler = require("express-async-handler");
 const { Transcation } = require("../../models");
+const { Account } = require("../../models");
+const { Currency } = require("../../models");
 
 module.exports = {
   findAll: asyncHandler(async (req, res) => {
@@ -20,13 +24,43 @@ module.exports = {
       description,
     } = req.body;
 
+    let exchange_rate = 1;
+    let converted_amount = amount;
+
+    const account = await Account.findByPk(account_id, { include: Currency });
+    const currency = await Currency.findByPk(currency_id);
+
+    if (!account) {
+      return res.status(404).json({ msg: "Account not found" });
+    }
+
+    if (account.currency_id !== currency_id) {
+      const transcationCurrency = currency;
+      const accountCurrency = account.currency;
+
+      if (!transcationCurrency || !accountCurrency) {
+        return res
+          .status(404)
+          .json({ msg: "Account or Transcation currency not found" });
+      }
+
+      const api = `https://v6.exchangerate-api.com/v6/${process.env.EXCHANGE_RATE_API_KEY}/pair/${transcationCurrency.code}/${accountCurrency.code}/${amount}`;
+
+      const res = await fetch(api);
+      const data = await res.json();
+
+      exchange_rate = data.conversion_rate;
+      converted_amount = data.converison_result;
+    }
+
     const transcation = await Transcation.create({
       account_id,
       category_id,
       transcation_type,
-      amount,
+      amount: converted_amount,
       currency_id,
       description,
+      exchange_rate,
     });
 
     return res.status(201).json(transcation);
