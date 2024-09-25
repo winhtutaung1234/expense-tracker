829cc8458793b897c65e1220dcbe44daa0f6d317
ToeOoWaiYan <toeoowaiyan4252@gmail.com>
2024-09-25 11:56:10 +0630

    Bro why are you back here

--- frontend/src/Services/Auth/index.ts
+++ frontend/src/Services/Auth/index.ts
@@ -1,7 +1,7 @@
 import {
-  ResendEmailResponse,
-  VerifyEmailProps,
-  VerifyEmailResponse,
+    ResendEmailResponse,
+    VerifyEmailProps,
+    VerifyEmailResponse,
 } from "../../Types/Auth/Email";
 import { LoginForm, LoginResponse } from "../../Types/Auth/Login";
 import { LogoutResponse } from "../../Types/Auth/Logout";
@@ -13,9 +13,6 @@ import Storage from "../Storage";
 
 class Auth {
     static async verify(): Promise<User> {
-        // if (!Storage.getItem('Access Token')) {
-        //     throw "No";
-        // }
         try {
             const response = await api.get<User>('/verify');
             return response.data;
@@ -29,92 +26,85 @@ class Auth {
             }
         }
     }
-    try {
-      const response = await api.get<User>("/verify");
-      return response.data;
-    } catch (error) {
-      Storage.clear();
-      throw error;
-    }
-  }
 
-  static async login(loginFormData: LoginForm): Promise<LoginResponse> {
-    try {
-      const response = await api.post<LoginResponse>("/login", loginFormData);
-      const { accessToken } = response.data;
-      if (accessToken) {
-        Storage.setItem("Access Token", accessToken);
-      }
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-  }
 
-  static async register(
-    registerFormData: RegisterForm
-  ): Promise<RegisterResponse> {
-    try {
-      const response = await api.post<RegisterResponse>(
-        "/users",
-        registerFormData
-      );
-      return response.data;
-    } catch (error) {
-      throw error;
+    static async login(loginFormData: LoginForm): Promise<LoginResponse> {
+        try {
+            const response = await api.post<LoginResponse>("/login", loginFormData);
+            const { accessToken } = response.data;
+            if (accessToken) {
+                Storage.setItem("Access Token", accessToken);
+            }
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
     }
-  }
 
-  static async logout(): Promise<LogoutResponse> {
-    try {
-      const response = await api.post<LogoutResponse>("/logout");
-      Storage.clear();
-      return response.data;
-    } catch (error) {
-      throw error;
+    static async register(
+        registerFormData: RegisterForm
+    ): Promise<RegisterResponse> {
+        try {
+            const response = await api.post<RegisterResponse>(
+                "/users",
+                registerFormData
+            );
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
     }
-  }
 
-  static async verifyEmail(
-    emailPayload: VerifyEmailProps
-  ): Promise<VerifyEmailResponse> {
-    try {
-      const response = await api.post<VerifyEmailResponse>(
-        `/email-verify`,
-        emailPayload
-      );
-      const { accessToken } = response.data;
-      if (accessToken) {
-        Storage.setItem("Access Token", accessToken);
-      }
-      return response.data;
-    } catch (error) {
-      throw error;
+    static async logout(): Promise<LogoutResponse> {
+        try {
+            const response = await api.post<LogoutResponse>("/logout");
+            Storage.clear();
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
     }
-  }
 
-  static async resendEmail(): Promise<ResendEmailResponse> {
-    try {
-      const response = await api.post<ResendEmailResponse>(
-        "/resend-verification"
-      );
-      return response.data;
-    } catch (error) {
-      throw error;
+    static async verifyEmail(
+        emailPayload: VerifyEmailProps
+    ): Promise<VerifyEmailResponse> {
+        try {
+            const response = await api.post<VerifyEmailResponse>(
+                `/email-verify`,
+                emailPayload
+            );
+            const { accessToken } = response.data;
+            if (accessToken) {
+                Storage.setItem("Access Token", accessToken);
+            }
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
     }
-  }
 
-  static async refreshToken(): Promise<RefreshTokenResponse> {
-    try {
-      const response = await api.post<RefreshTokenResponse>("/refresh-token");
-      const { accessToken } = response.data;
-      Storage.setItem("Access Token", accessToken);
-      return response.data;
-    } catch (error) {
-      console.error("Failed to refresh token:", error);
-      throw error;
+    static async resendEmail(): Promise<ResendEmailResponse> {
+        try {
+            const response = await api.post<ResendEmailResponse>(
+                "/resend-verification"
+            );
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
     }
-  }
+
+    static async refreshToken(): Promise<RefreshTokenResponse> {
+        try {
+            const response = await api.post<RefreshTokenResponse>("/refresh-token");
+            const { accessToken } = response.data;
+            Storage.setItem("Access Token", accessToken);
+            return response.data;
+        } catch (error) {
+            console.error("Failed to refresh token:", error);
+            throw error;
+        }
+    }
 }
 
 export default Auth;
